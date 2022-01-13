7a70b303e6e67bede9e6bb79c2498dc684f3d544
gosia-b <malgorzata.maria.bak@gmail.com>
2022-01-13 19:32:33 +0100

    Convert JSON to DataFrame

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from datetime import datetime, timedelta
 
+import pandas as pd
+
 from config import DATABASE_LOCATION, USER_ID, TOKEN
 
 
 if __name__ == "__main__":
+
+    # API request
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
@@ -18,5 +22,31 @@ if __name__ == "__main__":
     url = f"https://api.spotify.com/v1/me/player/recently-played?after={yesterday_unix_timestamp}"
     r = requests.get(url, headers=headers)
     data = r.json()
-    print(data)
-    # TODO: Convert "data" to a pandas DataFrame (import json?)
+
+    # Convert response: JSON to pandas DataFrame
+    song_name = []
+    album_name = []
+    album_year = []
+    artist = []
+    played_at = []
+    timestamp = []
+
+    for song in data["items"]:
+        song_name.append(song["track"]["name"])
+        album_name.append(song["track"]["album"]["name"])
+        album_year.append(song["track"]["album"]["release_date"][:4])
+        artist.append(song["track"]["album"]["artists"][0]["name"])
+        played_at.append(song["played_at"])
+        timestamp.append(song["played_at"][:10])
+
+    song_dict = {
+        "song_name": song_name,
+        "album_name": album_name,
+        "album_year": album_year,
+        "artist": artist,
+        "played_at": played_at,
+        "timestamp": timestamp
+    }
+
+    song_df = pd.DataFrame(song_dict)
+    print(song_df)
